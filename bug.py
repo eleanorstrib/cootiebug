769a982a1c3980997d0883df968fb6e535a37e4f
eleanorstrib <hello@eleanorstrib.com>
2017-04-23 14:47:01 -0700

    got callback on turn working, updated rules

--- bug.py
+++ bug.py
@@ -1,10 +1,13 @@
 
 from random import randint
+from threading import Timer
 
 from bug_data import rules
 
+
+
 def say_hello():
-    user_name = input("What's your name > ")
+    user_name = input("What's your name? > ")
     return user_name
 
 def start_selection():
@@ -12,12 +15,13 @@ def start_selection():
     return user_input
 
 def manager(start_selection):
+    user_name = say_hello()
     user_input = start_selection()
     if user_input.lower() == 'rules':
-        for item in rules:
-            print(item)
+        print(rules)
         start_selection()
     elif user_input.lower() == 'play':
+        roll_to_start(user_name)
         start_selection()
     else:
         print("Did not understand your command.")
@@ -31,8 +35,27 @@ def manager(start_selection):
 # instantiate the bug class for each
 # roll the dice, get a number, check
 
-def roll_to_start(player):
-    pass
+
+def roll_to_start(user_name):
+    print("Rolling the \U0001F3B2  for you, %s" % user_name)
+    user_name_result = randint(1,6)
+    print("You got %s" % user_name_result)
+    print("Now, I'll roll the \U0001F3B2  for me...")
+    computer_result = randint(1,6)
+    print("I got %s" % computer_result)
+    user_is_player_1 = True
+    if user_name_result >= computer_result:
+        print("Congratulations %s, you're player 1!" % user_name)
+        turn(roll_to_start(user_name), user_name)
+    else:
+        user_is_player_1 = False
+    return user_is_player_1
+
+def turn(roll_to_start, user_name):
+    if roll_to_start == True:
+        print(input("Ok %s, your turn! Type 'roll' to continue" % user_name))
+
+
 
 class Bug(object):
     def __init__(self):
@@ -57,8 +80,9 @@ class Bug(object):
         if value == 6:
             self.leg = True
 
-if __name__== '__main__':
-    print("\U0001F41B  " * 10, " * Welcome to CootieBug * ", "\U0001F41B  " * 10, )
-    manager(start_selection)
+if __name__ == "__main__":
+    def main():
+        print("\U0001F41B  " * 10, " * Welcome to CootieBug * ", "\U0001F41B  " * 10, )
+        manager(start_selection)
 
-main()
+    main()
--- bug_data.py
+++ bug_data.py
@@ -2,13 +2,10 @@
 # bug_dict = {
 #     'body': [1,'\U0002B55'],
 #     'head': [2,'\U0001F636'],
+#     'eyes': [2,''],
 #     'hat': [3,'\U0001F3A9'],
 #     'lips': [4,'\U0001F60A'],
 #     'legs': [5,0],
 #     }
 
-rules = ["The object of this game is to draw a completed cootie bug before the other players. Type 'play' to start; the program will roll the dice \U0001F3B2 \U0001F3B2 (generate a random number) for you and itself.  Whoever has the higher number will go first. ",
-            "Each player must get a one for the body then a two for the head, in that order. If not, the user forfeits their turn.  Once the head and body are added, other body parts can be added in any order. "
-            "After the head and the body, the player can get the rest of the parts in any order, but if the user rolls the number for a part they already have, they forfeit their turn. ",
-            "When players roll the number of a part they need, they get a free roll to attempt to get part they don't already have."
-        ]
+rules = " The object of this game is to draw a completed cootie bug before the other players.\r\r Type 'play' to start; the program will roll the dice \U0001F3B2  (generate a random number) for you and itself. \rWhoever has the higher number after the dice are rolled will go first. \r\r Each player must get a one for the body then a two for the head, in that order. If not, the user forfeits their turn.  \rOnce the head and body are added, other body parts can be added in any order.\r After the head and the body, the player can get the rest of the parts in any order, but if the user rolls the number for a part they already have, they forfeit their turn.\r\r When players roll the number of a part they need, they get a free roll to attempt to get part they don't already have."
